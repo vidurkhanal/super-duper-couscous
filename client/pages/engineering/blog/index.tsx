0d87ee61711e2b7dd279826806d73f804a839066
Sangit Manandhar <rux12@icloud.com>
2021-07-13 12:41:31 +0545

    About us

--- client/pages/engineering/blog/index.tsx
+++ client/pages/engineering/blog/index.tsx
@@ -116,28 +116,8 @@ const index: React.FC<{ posts: Post[] }> = ({ posts }) => {
         <VStack paddingTop="40px" spacing="2" alignItems="flex-start">
           <Heading as="h2">What we write about</Heading>
           <Text as="p" fontSize="lg">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            condimentum quam arcu, eu tempus tortor molestie at. Vestibulum
-            pretium condimentum dignissim. Vestibulum ultrices vitae nisi sed
-            imperdiet. Mauris quis erat consequat, commodo massa quis, feugiat
-            sapien. Suspendisse placerat vulputate posuere. Curabitur neque
-            tortor, mattis nec lacus non, placerat congue elit.
-          </Text>
-          <Text as="p" fontSize="lg">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            condimentum quam arcu, eu tempus tortor molestie at. Vestibulum
-            pretium condimentum dignissim. Vestibulum ultrices vitae nisi sed
-            imperdiet. Mauris quis erat consequat, commodo massa quis, feugiat
-            sapien. Suspendisse placerat vulputate posuere. Curabitur neque
-            tortor, mattis nec lacus non, placerat congue elit.
-          </Text>
-          <Text as="p" fontSize="lg">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            condimentum quam arcu, eu tempus tortor molestie at. Vestibulum
-            pretium condimentum dignissim. Vestibulum ultrices vitae nisi sed
-            imperdiet. Mauris quis erat consequat, commodo massa quis, feugiat
-            sapien. Suspendisse placerat vulputate posuere. Curabitur neque
-            tortor, mattis nec lacus non, placerat congue elit.
+            We post stupid shit for the moment. Think of better shit to write
+            here later.
           </Text>
         </VStack>
       </Container>
