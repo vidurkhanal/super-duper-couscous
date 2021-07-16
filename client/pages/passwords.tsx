fbffeddb2e42b377dfd26a2359c471bf295c0ed6
Vidur Khanal <65837961+vidurkhanal@users.noreply.github.com>
2021-07-15 22:30:47 -0400

    Passwords dashboard (#14)
    * prettier config
    
    * raleway and opensans added
    
    * fonts added to the chakra theme
    
    * few UX and security changes to passwords dashboard
    
    * nav made sticky
    
    * passwords dashboard some changes

--- client/pages/passwords.tsx
+++ client/pages/passwords.tsx
@@ -1,11 +1,77 @@
-import { Wrapper } from "../components/Home/Wrapper";
+import { Box, Text } from "@chakra-ui/react";
 import { Password } from "../components/Home/Password";
+import { Wrapper } from "../components/Home/Wrapper";
+import { sample_server_res } from "../types";
 
 const PasswordPage = () => {
+  const server_res: sample_server_res = {
+    data: [
+      {
+        category: "personal",
+        passwords: [
+          {
+            site: "account.apple.com",
+            username: "hello@123.co",
+            password: "password123",
+            strength: "GOOD",
+          },
+          {
+            site: "accounts.google.com",
+            username: "mydreams@google.co",
+            password: "Quit Dreaming And Get To Work",
+            strength: "GOOD",
+          },
+        ],
+      },
+      {
+        category: "payment and banking",
+        passwords: [
+          {
+            site: "apple.money.com",
+            username: "hello@123.co",
+            password: "password123",
+            strength: "GOOD",
+          },
+        ],
+      },
+
+      {
+        category: "business",
+        passwords: [
+          {
+            site: "yahoo.finance.com",
+            username: "mybusinessemail@domain.com",
+            password: "password123",
+            strength: "GOOD",
+          },
+        ],
+      },
+    ],
+  };
   return (
-    <Wrapper>
-      <Password />
-    </Wrapper>
+    <Box>
+      <Wrapper>
+        {server_res.data.map((item, index) => {
+          return (
+            <Box key={index}>
+              <Text
+                textTransform="capitalize"
+                fontSize="2xl"
+                mb="10px"
+                as="h1"
+                fontWeight="600"
+              >
+                {item.category}
+              </Text>
+              {item.passwords.map((item, index) => {
+                return <Password pass={item} key={index} />;
+              })}
+            </Box>
+          );
+        })}
+        {/* <Password /> */}
+      </Wrapper>
+    </Box>
   );
 };
 
