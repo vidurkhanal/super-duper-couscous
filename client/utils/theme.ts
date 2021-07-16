2f87a99aa627522a7cccd6d9a0a3d77a9c10a1bc
Vidur Khanal <vkhanal@caldwell.edu>
2021-07-15 21:31:52 -0400

    fonts added to the chakra theme

--- client/utils/theme.ts
+++ client/utils/theme.ts
@@ -10,6 +10,12 @@ const config: ThemeConfig = {
 };
 
 // 3. extend the theme
-const theme = extendTheme({ config });
+const theme = extendTheme({
+  config,
+  fonts: {
+    heading: "Open Sans",
+    body: "Raleway",
+  },
+});
 
 export default theme;
