e7136724d80a5a510cae543224a361d2913188df
Sangit Manandhar <rux12@icloud.com>
2021-07-21 17:56:50 +0545

    Add relations with credential column

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -4,9 +4,11 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from "typeorm";
+import { Credential } from "./credential";
 
 @ObjectType()
 @Entity()
@@ -33,4 +35,7 @@ export class User extends BaseEntity {
 
   @Column()
   password: string;
+
+  @OneToMany(() => Credential, (credential) => credential.user)
+  credentials: Credential[];
 }
