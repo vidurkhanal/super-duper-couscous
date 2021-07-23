d92ba8f28e206a638bac1cd2bd4afddc9eb36bfa
Sangit Manandhar <79345518+Sang-it@users.noreply.github.com>
2021-07-23 20:12:00 +0545

    Add email verification (#27)
    * Change read me
    
    * Add pass gen
    
    * Fixing merge conflicts
    
    * Fix heights in the landing page
    
    * Refactored the loops
    
    * Refactored it a little
    
    * Add darkmode and spinning generate button.
    
    * Fix mx spacing
    
    * Fix responsive mistakes
    
    * Change deg to turn for the refresh icon
    
    * Prettier config
    
    * Git igonore changes
    
    * Made responsive
    
    * About us
    
    * Add href to the blog title
    
    * Fix some merge bugs
    
    * Split the Code into Components
    
    * Initial pass comp mockup
    
    * Fix lints
    
    * Clean dev page
    
    * UI bug fix, change masterPin to alphanumeric
    
    * Add dotenv config
    
    * Add prettier config
    
    * Merge conflicts
    
    * Add credential column and its resolver
    
    * Add more envs
    
    * Graphql querry gen
    
    * Add relations with credential column
    
    * add decode and encode functions
    
    * Add big int library
    
    * Add a test in dev
    
    * Decode func for client
    
    * Add logout resolver
    
    * Add register page and its gql querry
    
    * Add email verification
    
    * Fix ts errors
    
    Co-authored-by: Vidur Khanal <65837961+vidurkhanal@users.noreply.github.com>

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -8,3 +8,4 @@ export const COOKIE_SECRET = process.env.SESSION_SECRET!;
 export const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY!;
 export const PUBLIC_KEY = process.env.PUBLIC_KEY!;
 export const PRIVATE_KEY = process.env.PRIVATE_KEY!;
+export const PAGE_URL = "http://localhost:8080";
