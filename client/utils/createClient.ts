7a522de4029f6058af3f9fa6b94272b29136c255
Sangit Manandhar <rux12@icloud.com>
2021-07-30 21:54:34 +0545

    Fix prod issue

--- client/utils/createClient.ts
+++ client/utils/createClient.ts
@@ -1,7 +1,12 @@
 import { cacheExchange, dedupExchange, fetchExchange } from "urql";
+import { __PROD__ } from "../constants";
+
+const url = __PROD__
+  ? "https://api.kpass12.ninja/graphql"
+  : "http://localhost:8080/graphql";
 
 export const URQLClient = (ssrExchange: any, _ctx: any) => ({
-  url: "https://api.kpass12.ninja/graphql",
+  url,
   fetchOptions: {
     credentials: "include" as const,
   },
