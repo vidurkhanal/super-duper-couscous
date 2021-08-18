5385a90a14295d9ad39d4330db348ea4433ba3fe
Vidur Khanal <vkhanal@caldwell.edu>
2021-08-18 12:38:43 -0400

    Fixed ENV and Decode Function

--- server/src/migrations/1629302741951-StrengthAndSiteName.ts
+++ server/src/migrations/1629302741951-StrengthAndSiteName.ts
@@ -1,11 +1,11 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class njjj1627570297593 implements MigrationInterface {
-    name = 'njjj1627570297593'
+export class StrengthAndSiteName1629302741951 implements MigrationInterface {
+    name = 'StrengthAndSiteName1629302741951'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "user" ("userID" uuid NOT NULL DEFAULT uuid_generate_v4(), "createDate" TIMESTAMP NOT NULL DEFAULT now(), "updatedDate" TIMESTAMP NOT NULL DEFAULT now(), "fullName" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "masterPIN" character varying, "isVerified" boolean NOT NULL DEFAULT false, CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "PK_46d78688eda2476cb18f7eae8a5" PRIMARY KEY ("userID"))`);
-        await queryRunner.query(`CREATE TABLE "credential" ("credentialID" uuid NOT NULL DEFAULT uuid_generate_v4(), "createDate" TIMESTAMP NOT NULL DEFAULT now(), "updatedDate" TIMESTAMP NOT NULL DEFAULT now(), "email" character varying NOT NULL, "password" text NOT NULL, "userID" uuid, CONSTRAINT "PK_e81ca0ac2403c42c5cdca54cee8" PRIMARY KEY ("credentialID"))`);
+        await queryRunner.query(`CREATE TABLE "credential" ("credentialID" uuid NOT NULL DEFAULT uuid_generate_v4(), "createDate" TIMESTAMP NOT NULL DEFAULT now(), "updatedDate" TIMESTAMP NOT NULL DEFAULT now(), "email" character varying NOT NULL, "password" text NOT NULL, "strength" integer NOT NULL, "siteName" character varying NOT NULL, "userID" uuid, CONSTRAINT "PK_e81ca0ac2403c42c5cdca54cee8" PRIMARY KEY ("credentialID"))`);
         await queryRunner.query(`ALTER TABLE "credential" ADD CONSTRAINT "FK_de38a6e66932cb484536e2ccf3a" FOREIGN KEY ("userID") REFERENCES "user"("userID") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
--- server/src/utility/passwordStrength.ts
+++ server/src/utility/passwordStrength.ts
@@ -3,15 +3,13 @@ export const passwordStrengthCalculator = (passwordToCheck: string): number => {
     "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
   );
   if (strongPassword.test(passwordToCheck)) {
-    return 3;
+    return 2;
   }
   let mediumPassword = new RegExp(
     "^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})"
   );
   if (mediumPassword.test(passwordToCheck)) {
-    return 2;
+    return 1;
   }
-  return 1;
+  return 0;
 };
-
-console.log(passwordStrengthCalculator("Hello@1223"));
