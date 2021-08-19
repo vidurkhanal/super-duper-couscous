e170634bb9bab2403da26e389049640a16ecb25c
Vidur Khanal <65837961+vidurkhanal@users.noreply.github.com>
2021-08-18 22:37:51 -0400

    Urql Caching (#52)
    * Reset ANd Forget
    
    * SOME DEVOPS THINGS
    
    * DOCKER FILE AND DOTENV SAFE
    
    * Removed sloppy UI errors
    
    * Joi Added to credentials
    
    * Fixed ENV and Decode Function
    
    * Delete next.config.js
    
    * URQL CACHING
    
    * Get Credentials query removed from dev path

--- client/pages/dev.tsx
+++ client/pages/dev.tsx
@@ -1,32 +1,8 @@
 import { withUrqlClient } from "next-urql";
-import AddCredModal from "../components/AddCredential/addCredModal";
-import { useGetCredentialsQuery, useMeQuery } from "../generated/graphql";
 import { URQLClient } from "../utils/createClient";
-import { decode } from "../utils/decode";
 
 const Dev = () => {
-  const [{ data }] = useGetCredentialsQuery();
-  console.log(data);
-  // const [{ data }] = useMeQuery();
-
-  if (data?.getCredentials)
-    console.log(decode(data.getCredentials[0].password));
-
-  // useEffect(() => {
-  //   const loginU = async () =>
-  //     await loginUser({
-  //       email: "vidur@vidur.com",
-  //       password: "vid123!@",
-  //     });
-  //   };
-  //   loginU();
-  // }, []);
-
-  return (
-    <div>
-      <AddCredModal />
-    </div>
-  );
+  return <div>Hi!!</div>;
 };
 
 export default withUrqlClient(URQLClient, { ssr: true })(Dev);
