74e08d9cfde964db85524db17b3710b766eb4a24
Sangit Manandhar <79345518+Sang-it@users.noreply.github.com>
2021-08-24 18:32:18 +0545

    Fix console logs (#66)
    * Fix Blurry Fav Icon
    
    * Removed console logs
    
    Co-authored-by: Vidur Khanal <65837961+vidurkhanal@users.noreply.github.com>

--- client/pages/dev.tsx
+++ client/pages/dev.tsx
@@ -4,6 +4,17 @@ import { useVerifyMasterPinMutation } from "../generated/graphql";
 import { useEffect } from "react";
 
 const Dev = () => {
+  const [, verifyMasterPIN] = useVerifyMasterPinMutation();
+
+  const fetch = async () => {
+    const { data, error } = await verifyMasterPIN({ masterPIN: "1234" });
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetch();
+  }, []);
+
   return <div>hey</div>;
 };
 
