36b806946b01171d896c5100336b5f07e5a041fe
Vidur Khanal <vkhanal@caldwell.edu>
2021-09-17 12:54:15 -0400

    Create Master Pin Almost Complete

--- client/pages/authentication/create-master-pin.tsx
+++ client/pages/authentication/create-master-pin.tsx
@@ -1,43 +1,65 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { Box, Text } from "@chakra-ui/layout";
-import { NextPage } from "next";
-import { withUrqlClient } from "next-urql";
-import { useMeQuery } from "../../generated/graphql";
-import { URQLClient } from "../../utils/createClient";
-import NextRouter from "next/router";
-import Head from "next/head";
-import { LoadingPage } from "../../components/Misc/LoadingPage";
 import {
   Button,
-  Checkbox,
   Flex,
   FormControl,
-  FormLabel,
   Heading,
-  Input,
-  Link,
-  Stack,
   Image,
+  Input,
+  Stack,
   useColorModeValue,
   useToast,
 } from "@chakra-ui/react";
+import { NextPage } from "next";
+import { withUrqlClient } from "next-urql";
+import Head from "next/head";
+import NextRouter from "next/router";
+import {
+  ChangeEvent,
+  Dispatch,
+  FormEvent,
+  SetStateAction,
+  useState,
+} from "react";
+import { LoadingPage } from "../../components/Misc/LoadingPage";
 import {
   BRAND_COLOR_RED,
   HOVER_BRAND_COLOR_RED,
   MODAL_DARK_BACKGROUND,
 } from "../../constants";
-import { ChangeEvent, Dispatch, SetStateAction, useState } from "react";
+import {
+  useCreateMasterPinMutation,
+  useMeQuery,
+} from "../../generated/graphql";
+import { URQLClient } from "../../utils/createClient";
+import { useRouter } from "next/router";
 
 const CreateMasterPin: NextPage = () => {
   const [{ data, fetching }] = useMeQuery();
   const [masterPIN, setMasterPIN] = useState("");
   const [reMasterPIN, setReMasterPIN] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [, createMasterPIN] = useCreateMasterPinMutation();
   const toast = useToast();
+  const router = useRouter();
 
-  const handleSubmit = async () => {
-    // LOGIC FOR SUBMITTING...
-    // I HAVEN'T LINKED THE BACKEND< DO IT IF YOU WANT...
-    // I HAVE GENERATED THE GQL FOR YOU THO :)
+  const formResetter = () => {
+    setMasterPIN("");
+    setReMasterPIN("");
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    if (masterPIN !== reMasterPIN) {
+      return toast({
+        title: "Your PINs don't match. Please enter the same PIN.",
+        status: "error",
+        duration: 2000,
+        isClosable: true,
+      });
+    }
     let flag = false;
     const arr = masterPIN.split("");
 
@@ -53,13 +75,44 @@ const CreateMasterPin: NextPage = () => {
         isClosable: true,
       });
     }
+
+    const { data, error } = await createMasterPIN({ masterPIN });
+    if (error) {
+      toast({
+        title: error.message,
+        status: "error",
+      });
+    }
+
+    if (data?.createMasterPIN.error) {
+      toast({
+        title: data.createMasterPIN.error,
+        status: "error",
+      });
+      setIsSubmitting(false);
+    }
+
+    if (data?.createMasterPIN.isSuccessful) {
+      toast({
+        title: "Master PIN set.",
+        description: "Redirecting you to your dashboard.",
+        status: "success",
+        isClosable: true,
+        duration: 5000,
+      });
+      formResetter();
+      setIsSubmitting(false);
+      return setTimeout(() => {
+        router.reload();
+      }, 5000);
+    }
   };
 
   const changeStatesForMasterPIN = (
     e: ChangeEvent<HTMLInputElement>,
     setterFn: Dispatch<SetStateAction<string>>
   ) => {
-    // I GOT BORED TO WRITE ASCII CODE FOR MASTER PIN
+    setterFn(e.target.value);
   };
 
   if (!fetching && !data?.me) {
@@ -84,7 +137,16 @@ const CreateMasterPin: NextPage = () => {
               src={"/create-master-pin.jpeg"}
             />
           </Flex>
-          <Flex p={8} flex={1} align={"center"} justify={"center"}>
+          <Flex
+            p={8}
+            flex={1}
+            align={"center"}
+            justify={"center"}
+            bg={{
+              base: "linear-gradient(rgba(0,0,0,0.85),rgba(0,0,0,0.85)),url('/create-master-pin.jpeg')",
+              md: "none",
+            }}
+          >
             <Stack
               spacing={4}
               w={"full"}
@@ -102,16 +164,18 @@ const CreateMasterPin: NextPage = () => {
                 fontSize={{ base: "sm", sm: "md" }}
                 color={useColorModeValue("gray.800", "gray.400")}
               >
-                You can create it right here.
+                You can create it right here. Enter your 4 character PIN.
               </Text>
-              <form>
+              <form onSubmit={handleSubmit}>
                 <FormControl mb={5}>
                   <Input
                     placeholder="Enter Your Master PIN"
                     _placeholder={{ color: "gray.500" }}
-                    type="email"
+                    type="password"
                     required
                     value={masterPIN}
+                    name="Master PIN"
+                    maxLength={4}
                     onChange={(e) => changeStatesForMasterPIN(e, setMasterPIN)}
                   />
                 </FormControl>
@@ -119,8 +183,9 @@ const CreateMasterPin: NextPage = () => {
                   <Input
                     placeholder="Re Enter Your Master PIN"
                     _placeholder={{ color: "gray.500" }}
-                    type="email"
+                    type="password"
                     required
+                    maxLength={4}
                     value={reMasterPIN}
                     onChange={(e) =>
                       changeStatesForMasterPIN(e, setReMasterPIN)
@@ -135,6 +200,8 @@ const CreateMasterPin: NextPage = () => {
                     _hover={{
                       bg: HOVER_BRAND_COLOR_RED,
                     }}
+                    isLoading={isSubmitting}
+                    disabled={isSubmitting}
                   >
                     Set Master PIN
                   </Button>
